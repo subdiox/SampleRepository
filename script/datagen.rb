1f5f579dfce80ee26eef0c1ba4023435c7203806
subdiox <subdiox@gmail.com>
2018-01-13 01:06:22 +0900

    update scripts

--- script/datagen.rb
+++ script/datagen.rb
@@ -1,115 +1,58 @@
-require 'rbconfig'
+require 'open3'
 require 'fileutils'
+require 'net/http'
+require 'uri'
 
-def getOSType
-    os = "unknown"
-    bit = 64
-    case RbConfig::CONFIG['host_os']
-    when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
-        os = "windows"
-        if `echo %PROCESSOR_ARCHITECTURE%` == "x86"
-            bit = 32
-        end
-    when /darwin|mac os/
-        os = "mac"
-        if `uname -a | grep i386` != ""
-            bit = 32
-        end
-    when /linux|solaris|bsd/
-        os = "linux"
-        if `uname -a | grep i686` != "" || `uname -a | grep i386` != ""
-            bit = 32
-        end
-    end
-    return os,bit
+#############################################################################################
+# You need ResourcePatcher binary from DAVA Engine (https://github.com/dava/dava.engine)
+# Compiled binary is uploaded on subdiox repository (https://github.com/subdiox/dava.engine)
+#
+# - Path to ResourcePatcher binary (executable format)
+rp_path = "/Applications/Toolset/ResourcePatcher"
+#############################################################################################
+
+
+if !File.exist?("wotblitz.apk")
+    puts "Downloading wotblitz.apk ..."
+    command = "curl -s -L \'https://apkpure.com/world-of-tanks-blitz/net.wargaming.wot.blitz/download\' | grep id=\\\"iframe_download | cut -d\'\"\' -f4"
+    o, e, s = Open3.capture3(command)
+
+    url = o.chop
+    puts "URL: " + url
+    command = "wget -O wotblitz.apk \'" + url + "\'"
+    Open3.pipeline(command)
+    puts "Finished downloading."
 end
 
-if !File.exist?("Data")
-    puts "'Data' directory does not exist in the current directory."
-    exit
+if !File.exist?("wotblitz/assets/Data/version.txt")
+    puts "Unzipping wotblitz.apk to wotblitz ..."
+    command = "rm -rf wotblitz && unzip -d wotblitz wotblitz.apk"
+    o, e, s = Open3.capture3(command)
+    puts "Finished unzipping."
 end
 
-if !File.exist?("pvr.tex")
-    puts "pvr.tex does not exist in the current directory."
-    exit
+command = "cat wotblitz/assets/Data/version.txt | cut -d'.' -f1-3"
+o, e, s = Open3.capture3(command)
+version = o.chop
+
+command = "curl \'http://dl-wotblitz-gc.wargaming.net/dlc/g" + version + ".info\'"
+o, e, s = Open3.capture3(command)
+patch_number = o
+
+puts "--------------------------------"
+puts "   Version: " + version
+puts "   Patch Number: " + patch_number
+puts "--------------------------------"
+
+if !File.exist?("patch")
+    puts "Downloading patch file ..."
+    url = "http://dl-wotblitz-gc.wargaming.net/dlc/r" + patch_number + "/r0-" + patch_number + ".patch"
+    command = "wget -O patch \'" + url + "\'"
+    Open3.pipeline(command)
+    puts "Finished downloading."
 end
 
-if !File.exist?("webp.tex")
-    puts "webp.tex does not exist in the current directory."
-    exit
-end
-
-os, bit = getOSType()
-toolPath = ""
-pvrPath = []
-if os == "windows" && bit == 64
-    toolPath = "PVRTexTool\\Windows_x86_64\\PVRTexTool.exe"
-    pvrArray = Dir.glob("Data\\**\\*.PowerVR_iOS.pvr")
-    webpiOSArray = Dir.glob("Data\\**\\*.PowerVR_iOS.webp")
-elsif os == "windows" && bit == 32
-    toolPath = "PVRTexTool\\Windows_x86_32\\PVRTexTool.exe"
-    pvrArray = Dir.glob("Data\\**\\*.PowerVR_iOS.pvr")
-    webpiOSArray = Dir.glob("Data\\**\\*.PowerVR_iOS.webp")
-elsif os == "mac"
-    toolPath = "PVRTexTool/OSX_x86/PVRTexTool"
-    pvrArray = Dir.glob("Data/**/*.PowerVR_iOS.pvr")
-    webpiOSArray = Dir.glob("Data/**/*.PowerVR_iOS.webp")
-    system("chmod +x " + toolPath)
-elsif os == "linux" && bit == 64
-    toolPath = "PVRTexTool/Linux_x86_64/PVRTexTool"
-    pvrArray = Dir.glob("Data/**/*.PowerVR_iOS.pvr")
-    webpiOSArray = Dir.glob("Data/**/*.PowerVR_iOS.webp")
-    system("chmod +x " + toolPath)
-elsif os == "linux" && bit == 32
-    toolPath = "PVRTexTool/Linux_x86_32/PVRTexTool"
-    pvrArray = Dir.glob("Data/**/*.PowerVR_iOS.pvr")
-    webpiOSArray = Dir.glob("Data/**/*.PowerVR_iOS.webp")
-    system("chmod +x " + toolPath)
-end
-
-if !File.exist?(toolPath)
-    puts "Appropriate PVRTexTool does not exist in the current directory."
-    exit
-end
-
-puts "This script converts all iOS_PowerVR.pvr files into mali.pvr in the Data directory."
-puts "Which mali format do you want to convert into? (1.RGBA4444  2.RGBA8888) [Default:1]"
-format = gets.to_s
-if format != "1" && format != "2"
-    format = "1"
-end
-
-failArray = []
-for pvrPath in pvrArray do
-    maliPath = pvrPath.gsub("\.PowerVR_iOS\.pvr", "\.mali\.pvr")
-    texPath = pvrPath.gsub("\.PowerVR_iOS\.pvr", "\.tex")
-    if format == "1"
-        result = system(toolPath + " -i '" + pvrPath + "' -o '" + maliPath + "' -f r4g4b4a4,UBN,lRGB")
-        if result
-            File.delete(pvrPath)
-            FileUtils.copy("pvr.tex", texPath)
-        else
-            failArray.push(pvrPath)
-        end
-    elsif format == "2"
-        result = system(toolPath + " -i '" + pvrPath + "' -o '" + maliPath + "' -f r8g8b8a8,UBN,lRGB")
-        if result
-            File.delete(pvrPath)
-            FileUtils.copy("pvr.tex", texPath)
-        else
-            failArray.push(pvrPath)
-        end
-    end
-end
-
-for webpiOSPath in webpiOSArray do
-    webpPath = webpiOSPath.gsub("\.PowerVR_iOS\.webp", "\.webp")
-    FileUtils.mv(webpiOSPath, webpPath)
-    texPath = webpiOSPath.gsub("\.PowerVR_iOS\.webp", "\.tex")
-    FileUtils.copy("webp.tex", texPath)
-end
-
-if failArray.count > 0
-    puts "Following file(s) could not be converted:"
-    puts failArray
-end
+puts "Applying patch file ..."
+command = "rm -rf Data && cp -r wotblitz/assets/Data . && cd Data && " + rp_path + " apply-all ../patch"
+Open3.pipeline(command)
+puts "Finished applying."
--- script/removegen.rb
+++ script/removegen.rb
@@ -1,111 +1,89 @@
 require 'rubygems'
 require 'rbconfig'
 require 'fileutils'
+require 'digest'
+require 'csv'
 require 'archive/zip'
 
-def getOSType
-    os = "unknown"
-    bit = 64
-    case RbConfig::CONFIG['host_os']
-    when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
-        os = "windows"
-        if `echo %PROCESSOR_ARCHITECTURE%` == "x86"
-            bit = 32
-        end
-    when /darwin|mac os/
-        os = "mac"
-        if `uname -a | grep i386` != ""
-            bit = 32
-        end
-    when /linux|solaris|bsd/
-        os = "linux"
-        if `uname -a | grep i686` != "" || `uname -a | grep i386` != ""
-            bit = 32
-        end
-    end
-    return os,bit
-end
-
-os, bit = getOSType()
-
 if !File.exist?("Data")
-    puts "'Data' directory does NOT exist in the current directory."
+    puts "[Error] 'Data' directory does not exist in the current directory."
     exit
 end
 
-installDir = "../install"
-if os == "windows"
-    installDir = "..\\install"
-end
-if !File.exist?(installDir)
-    puts "'install' directory does NOT exist in the current directory."
+if !File.exist?("../install")
+    puts "[Error] 'install' directory does not exist in the parent directory."
     exit
 end
 
-puts "This script generates remove directory from Data directory and install directory."
+puts "[#] This script generates removal files from original Data and installation files."
 
-if os == "windows"
-    if File.exist?("..\\remove")
-        FileUtils.rm_rf("..\\remove")
-        Dir.mkdir("..\\remove")
-    end
-    puts "Cleared remove directory."
-
-    zipArray = Dir.glob("..\\install\\**\\*.zip")
-    for installZipPath in zipArray do
-        puts "Processing " + installZipPath.gsub("\.\.\\\\install\\\\", "") + " ..."
-        Archive::Zip.extract(installZipPath, "..\\install")
-        fileArray = Dir.glob("..\\install\\Data\\**\\*")
-        for installFile in fileArray do
-            if File::ftype(installFile) != "directory"
-                dataFile = installFile.gsub("\.\.\\\\install\\\\", "")
-                removeFile = installFile.gsub("\.\.\\\\install\\\\", "\.\.\\\\remove\\\\")
-                removeFileArray = removeFile.split('\\')
-                removeFileDir = (removeFileArray.first removeFileArray.size - 1).join("\\")
-                if !File.exist?(removeFileDir)
-                    FileUtils.mkdir_p(removeFileDir)
-                end
-                if File.exist?(dataFile)
-                    FileUtils.copy(dataFile, removeFile)
-                end
-            end
-        end
-        FileUtils.rm_rf("..\\install\\Data")
-        removeZipPath = installZipPath.gsub("\.\.\\\\install\\\\", "\.\.\\\\remove\\\\")
-        Archive::Zip.archive(removeZipPath, "..\\remove\\Data")
-        FileUtils.rm_rf("..\\remove\\Data")
-    end
-    puts "Completed generating remove directory."
-else
-    if File.exist?("../remove")
-        FileUtils.rm_rf("../remove")
-        Dir.mkdir("../remove")
-    end
-    puts "Cleared remove directory."
-
-    zipArray = Dir.glob("../install/**/*.zip")
-    for installZipPath in zipArray do
-        puts "Processing " + installZipPath.gsub("\.\./install/", "") + " ..."
-        Archive::Zip.extract(installZipPath, "../install")
-        fileArray = Dir.glob("../install/Data/**/*")
-        for installFile in fileArray do
-            if File::ftype(installFile) != "directory"
-                dataFile = installFile.gsub("\.\./install/", "")
-                removeFile = installFile.gsub("\.\./install/", "\.\./remove/")
-                removeFileArray = removeFile.split('/')
-                removeFileDir = (removeFileArray.first removeFileArray.size - 1).join("/")
-                if !File.exist?(removeFileDir)
-                    FileUtils.mkdir_p(removeFileDir)
-                end
-                if File.exist?(dataFile)
-                    FileUtils.copy(dataFile, removeFile)
-                end
-            end
-        end
-        FileUtils.rm_rf("../install/Data")
-        removeZipPath = installZipPath.gsub("\.\./install/", "\.\./remove/")
-        Archive::Zip.archive(removeZipPath, "../remove/Data")
-        FileUtils.rm_rf("../remove/Data")
-    end
-    puts "Completed generating remove directory."
+if File.exist?("../.tmp")
+    FileUtils.rm_rf("../.tmp")
 end
+FileUtils.mkdir_p("../.tmp/install")
+FileUtils.mkdir_p("../.tmp/remove")
+
+checksums_file = 'checksums.csv'
+checksums = []
+if File.exists?(checksums_file)
+    checksums = CSV.read(checksums_file)
+end
+
+str = ""
+Dir.glob("../install/*.zip").sort.each do |install_zip_path|
+    install_zip_file = File.basename(install_zip_path)
+
+    last_str = str
+    str = "[#] Processing #{install_zip_file} ..."
+    diff = last_str.length - str.length
+    if diff < 0 then
+        diff = 0
+    end
+    print(str + " " * diff + "\r")
+    STDOUT.flush
+
+    checksum = Digest::MD5.file(install_zip_path).hexdigest
+    search_result = checksums.select{ |c| c[0] == install_zip_file }
+    if search_result.count == 0 then
+        checksums.push([install_zip_file, checksum])
+    elsif search_result.count == 1 then
+        if search_result[0][1] == checksum then
+            next
+        end
+    else
+        checksums.keep_if{ |c| c[0] == install_zip_file }
+        checksums.push([install_zip_file, checksum])
+    end
+
+    Archive::Zip.extract(install_zip_path, "../.tmp/install")
+    if Dir.glob("../.tmp/install/*").map{|path| File.basename(path)}.include?("Data") then
+        Dir.glob("../.tmp/install/Data/**/*").each do |install_path|
+            if File::ftype(install_path) != "directory"
+                data_file = install_path.gsub("\.\./\.tmp/install", "")
+                remove_path = install_path.gsub("\.\./\.tmp/install/", "\.\./\.tmp/remove/")
+                remove_dir = File.dirname(remove_path)
+                if !File.exist?(remove_dir)
+                    FileUtils.mkdir_p(remove_dir)
+                end
+                if File.exist?(data_file)
+                    FileUtils.copy(data_file, remove_path)
+                end
+            end
+        end
+        remove_zip_path = install_zip_path.gsub("\.\./install/", "\.\./remove/")
+        Archive::Zip.archive(remove_zip_path, "../.tmp/remove/Data")
+        FileUtils.rm_rf(Dir.glob("../.tmp/install/*"))
+        FileUtils.rm_rf(Dir.glob("../.tmp/remove/*"))
+    else
+        FileUtils.rm_rf("../.tmp/")
+        puts "[Error] Data directory is not included in #{install_zip_file}"
+        exit
+    end
+end
+FileUtils.rm_rf("../.tmp/")
+CSV.open(checksums_file, 'w') do |c_f|
+    checksums.sort.each do |c|
+        c_f << c
+    end
+end
+puts "[#] Completed generating remove directory."
